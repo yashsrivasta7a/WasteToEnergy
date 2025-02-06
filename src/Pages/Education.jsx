ef5742186dfe9335e58153774985519be880d84f
IAmDevGarg <128077358+IAmDevGarg@users.noreply.github.com>
2025-02-07 00:56:12 +0530

    education-css

--- src/Pages/Education.jsx
+++ src/Pages/Education.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import EducationData from "../components/EducationData";
+import "./Education.css"
 
 const EducationPage = () => {
   return (
+    <>
+    <h1 id="EduTitle">Educational Centre</h1>
     <div className="edu-container">
-        <h1>Educational Centre</h1>
       <h1>{EducationData.waste_to_energy.title}</h1>
       {
       EducationData.waste_to_energy.sections.map((section, index) => (
-        <div key={index}>
+        <div key={index} className="edu-card">
           <h2>{section.title}</h2>
           <p>{section.content}</p>
         </div>
       ))}
     </div>
+    </>
   );
 };
 
