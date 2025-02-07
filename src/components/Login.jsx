deeb3c3ae4f74715fbe3b763cb96c111ef7bd387
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2025-02-07 10:36:14 +0530

    Update Login button styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,14 +6,15 @@ const LoginButton = () => {
 
   return <button style={{
     marginTop: "20px",
-    padding: "10px 20px",
+    padding: "10px 300px",
     border: "none",
     backgroundColor: "#90ee90",
     color: "#333",
     fontSize: "1rem",
     fontWeight: "900",
     cursor: "pointer",
-    borderRadius: "5px"
+    borderRadius: "5px",
+    left:"20px"
 }} onClick={() => loginWithRedirect()}>Login / SignUp</button>;
 };
 
