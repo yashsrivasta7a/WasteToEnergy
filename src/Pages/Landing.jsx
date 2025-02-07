5117dbe95c15208c03c61405151c3245e13e448a
manasjhamj <jhamanas939@gmail.com>
2025-02-08 02:58:23 +0530

    Minor fixes

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -3,12 +3,14 @@ import './Landing.css';
 import background from '../assets/background.png';
 import illustration from '../assets/illustration.png';
 import LoginButton from '../components/Login';
+import logo from '../assets/garshh.png';
 
 
 function Landing() {
     return (
         <div className="container">
             <div className="text-section">
+                <img src={logo} alt="Garshh Logo" className="logo" />
                 <h1 className="heading">
                     Help transform <span className="highlight">waste</span> into energy and get rewarded
                     with points for your <span className="highlight">eco-friendly</span> efforts.
